5edf28f5f5e5d41a0dc38bf6fd0dac4ad6bda3d2
Elinaioa <elinaioa2005@gmail.com>
2026-01-30 08:46:44 +0000

    completed lab

---
+++ FredsBoats.Web/Models/Comment.cs
@@ -0,0 +1,11 @@
+namespace FredsBoats.Web.Models
+{
+    public class Comment{
+        public int CommentId {get; set;}
+        public string Content{get; set;}
+        public string Author {get; set;}
+        public DateTime CreatedAt{get; set;}
+        public int BoatId {get; set;}
+        public Boat? Boat {get; set;}
+    }
+}
